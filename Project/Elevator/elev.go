38d389e99ca7dfc1e7cadb41eb0351d0ea178882
hichamel <hicham.elharem@telecom-bretagne.eu>
2016-03-10 21:56:16 +0100

    Create elev.go
    new elev.go (not finished yet)

---
+++ Project/Elevator/elev.go
@@ -0,0 +1,100 @@
+package elevator
+import (
+)
+
+const N_FlOORS = 4
+const N_BUTTONS = 4
+
+type Elev_button int
+
+var Lamp_channel_matrix = [N_FLOORS][N_BUTTONS]int{
+	{driver.LIGHT_UP1, driver.LIGHT_DOWN1, driver.LIGHT_COMMAND1},
+	{driver.LIGHT_UP2, driver.LIGHT_DOWN2, driver.LIGHT_COMMAND2},
+	{driver.LIGHT_UP3, driver.LIGHT_DOWN3, driver.LIGHT_COMMAND3},
+	{driver.LIGHT_UP4, driver.LIGHT_DOWN4, driver.LIGHT_COMMAND4},
+}
+
+var Button_channel_matrix = [N_FLOORS][N_BUTTONS]int{
+	{driver.FLOOR_UP1, driver.FLOOR_DOWN1, driver.FLOOR_COMMAND1},
+	{driver.FLOOR_UP2, driver.FLOOR_DOWN2, driver.FLOOR_COMMAND2},
+	{driver.FLOOR_UP3, driver.FLOOR_DOWN3, driver.FLOOR_COMMAND3},
+	{driver.FLOOR_UP4, driver.FLOOR_DOWN4, driver.FLOOR_COMMAND4},
+}
+
+func SetElevSpeed
+func SetElevFloorIndicator(floor int) {
+	//one light sould be on
+	if floor == 1 || floor == 2 {
+		driver.Set_bit(driver.FLOOR_IND1)
+	} else {
+		driver.Clear_bit(driver.FLOOR_IND1)
+	}
+	if floor == 3 || floor == 4 {
+		driver.Set_bit(driver.FLOOR_IND2)
+	} else {
+		driver.Clear_bit(driver.FLOOR_IND2)
+	}
+}
+func SetElevButtonLamp(button Elev_button, floor int, value int) {
+	if value == 1 {
+		driver.Set_bit(Lamp_channel_matrix[floor][button])
+	} else {
+		driver.Clear_bit(Lamp_channel_matrix[floor][button])
+	}
+}
+func SetElevDoorOpenLamp(value int) {
+	if value == 1 {
+		driver.Set_bit(driver.DOOR_OPEN)
+	} else {
+		driver.Clear_bit(driver.DOOR_OPEN)
+	}
+}
+func SetElevStopLamp(value int) {
+	if value == 1 {
+		driver.Set_bit(drivers.LIGHT_STOP)
+	} else {
+		driver.Clear_bit(drivers.LIGHT_STOP)
+	}
+}
+func GetElevFloorSensorSignal() int {
+	if driver.Read_bit(driver.SENSOR1) {
+		return 1
+	} else if driver.Read_bit(driver.SENSOR2) {
+		return 2
+	} else if driver.Read_bit(driver.SENSOR3) {
+		return 3
+	} else if driver.Read_bit(driver.SENSOR4) {
+		return 4
+	} else {
+		return -1
+	}
+
+}
+
+func GetElevButtonSignal(button Elev_button, floor int) int {
+	if driver.Read_bit(Button_channel_matrix[floor][button]) {
+		return 1
+	} else {
+		return 0
+	}
+}
+func GetElevStopSignal() bool {
+	return driver.Read_bit(driver.STOP)
+}
+func GetElevObstructionSignal() bool {
+		return driver.Read_bit(driver.OBSTRUCTION)
+}
+func ElevInit() {
+	// Zero all floor button lamps
+	for i := 1; i < N_FLOORS; i++ {
+		for b:=0;b<N_BUTTONS;b++ {
+			SetElevButtonLamp(b, i, 0)
+		}		
+	}
+
+	// Clear stop lamp, door open lamp, and set floor indicator to ground floor.
+	ElevSetStopLamp(0)
+	ElevSetDoorOpenLamp(0)
+	ElevSetFloorIndicator(0)
+
+}
